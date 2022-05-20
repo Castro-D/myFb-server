c26d3cca5fe4f8a41d32db446834d25657e3e362
castro-d <castro.dh91@gmail.com>
2022-05-20 09:01:36 -0300

    add example queries

--- src/app.js
+++ src/app.js
@@ -1,26 +1,57 @@
 const express = require('express');
+const { Pool } = require('pg');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Facebook',
+  password: 'admin',
+  port: 5432,
 });
 
-app.get('/posts', (req, res) => {
-  res.json({ message: 'Got all posts!' });
+app.get('/posts', (req, result) => {
+  pool.query('SELECT * FROM posts', (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      result.json(res.rows);
+    }
+  });
 });
 
-app.post('/posts', (req, res) => {
-  res.json({ message: 'post created' });
+app.post('/posts', (req, result) => {
+  pool.query('INSERT INTO posts (comment, img_path) values ($1, $2)', ['posted from node', 'testing/path'], (err) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      result.json({ message: 'post succesful' });
+    }
+  });
 });
 
-app.put('/posts/id', (req, res) => {
-  res.json({ message: 'updated a post' });
+app.put('/posts/:id', (req, result) => {
+  const { id } = req.params;
+  pool.query('UPDATE posts SET comment=$1, img_path=$2 WHERE id=$3', ['asdasdas another example', 'test', id], (err) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      result.json({ message: 'post updated succesfully' });
+    }
+  });
 });
 
-app.delete('/posts/id', (req, res) => {
-  res.json({ message: 'deleted a post' });
+app.delete('/posts/:id', (req, result) => {
+  const { id } = req.params;
+  pool.query('DELETE FROM posts WHERE id = $1', [id], (err) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      result.json({ message: 'post deleted succesfully' });
+    }
+  });
 });
 
 app.listen(port, () => {
